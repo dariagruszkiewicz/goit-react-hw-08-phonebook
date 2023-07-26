e03c6be8743074ce6000fe61e28d031526aa884a
Daria Gruszkiewicz <dariaa.pajak@gmail.com>
2023-07-26 22:18:23 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,23 +3,65 @@ import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import css from './App.module.css';
 import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { lazy, useEffect } from 'react';
 import { fetchContacts } from 'redux/operations';
+import { Routes, Route } from 'react-router-dom';
+import Home from 'pages/Home';
+import Login from 'pages/Login';
+import Register from 'pages/Register';
+import Contacts from 'pages/Contacts';
+import { Layout } from './Layout/Layout';
+import { RestrictedRoute } from './RestrictedRoute/RestrictedRoute';
+import { PrivateRoute } from './PrivateRoute/PrivateRoute';
+
+const HomePage = lazy(() => import('../pages/Home'));
+const RegisterPage = lazy(() => import('../pages/Register'));
+const LoginPage = lazy(() => import('../pages/Login'));
+const ContactsPage = lazy(() => import('../pages/Contacts'));
 
 export const App = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(fetchContacts());
+  // }, [dispatch]);
 
   return (
     <div className={css.wrapper}>
-      <h1>Phonebooks</h1>
+      {/* <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
       <Filter />
-      <ContactList />
+      <ContactList /> */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute
+                redirectTo="/contacts"
+                component={<LoginPage />}
+              />
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute
+                redirectTo="/contacts"
+                component={<RegisterPage />}
+              />
+            }
+          />
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+            }
+          />
+        </Route>
+      </Routes>
     </div>
   );
 };
---
+++ src/pages/Contacts.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from 'react-helmet';
+
+const Contacts = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Contacts</title>
+      </Helmet>
+      <p>jchdbgvefhgrthyuji</p>
+    </>
+  );
+};
+
+export default Contacts;
